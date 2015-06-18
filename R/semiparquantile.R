746dae80d54944cd0e4fca5e6f042166509eb309
Stefan Wager <swager@stanford.edu>
2015-06-18 02:24:35 -0400

    quantiles

--- R/semiparquantile.R
+++ R/semiparquantile.R
@@ -12,25 +12,21 @@ semipar.quantile <- function(main.sample,
                          threshold=NULL) {
   # precompute semiparametric estimate of the cumulative distribution function
   semipar.est <- semipar.tail(main.sample, background.sample, threshold)
-  cdf <- cumsum(semipar.est$weights)
+  semipar.ord <- semipar.est[order(semipar.est$X),]
+  semipar.cdf <- cumsum(semipar.ord$weights)
   
   quantiles <- numeric(length(probs))
   for(i in 1:length(probs)) {
     p <- probs[i]
-    if(p < 0 || p > 1) {
+    if(p <= 0 || p >= 1) {
       stop("probs must be between 0 and 1")
     }
-    
-    # if p is in between two atoms, interpolate them
-    if(p %in% cdf[1:(length(cdf)-1)]) {
-      which.atom <- min(which(cdf == p))
-      quantiles[i] <- mean(semipar.est$X[which.atom + 0:1])
-    } 
-    # otherwise, use the value for the first atom whose cdf exceeds p
-    else {
-      which.atom <- 1 + sum(cumsum(semipar.est$weights) < p)
-      quantiles[i] <- semipar.est$X[which.atom]      
+    if(p < min(semipar.cdf) || p >= max(semipar.cdf)) {
+      stop("quantiles outside of range of estimated distribution")
     }
+    
+    which.atom <- 1 + sum(semipar.cdf < p)
+    quantiles[i] <- semipar.ord$X[which.atom]   
   }
   return(quantiles)
 }
